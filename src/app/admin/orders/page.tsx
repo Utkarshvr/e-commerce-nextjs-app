ffb94dfd010cd47f873a99bc90f07d0cbfda2c03
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-06-24 22:46:32 +0530

    orders page table compelted

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,26 @@
+import OrdersTable from "@/components/admin/orders/table/OrdersTable";
+
+async function getData(): Promise<any[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "12345",
+      customerID: "53221",
+      createdAt: "08/12/2022 11:28PM",
+      value: "1455",
+      status: "pending",
+      items: 2,
+    },
+  ];
+}
+
+export default async function Page() {
+  const data = await getData();
+
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-bold mb-2">Orders</h1>
+      <OrdersTable data={data} />
+    </div>
+  );
+}
