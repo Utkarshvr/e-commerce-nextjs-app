0d6eb3a58bbc1de2ccadcfb42f7bebf35f6616b5
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2024-06-27 21:32:41 +0530

    Login & SignUp Page

--- src/app/admin/(auth)/sign-in/page.tsx
+++ src/app/admin/(auth)/sign-in/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
@@ -7,14 +6,11 @@ import { Label } from "@/components/ui/label";
 
 export default function page() {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-      <div className="flex items-center justify-center py-12">
+    <div className="w-full h-screen">
+      <div className="flex w-full h-full items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Login</h1>
-            <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
-            </p>
           </div>
           <div className="grid gap-4">
             <div className="grid gap-2">
@@ -36,7 +32,12 @@ export default function page() {
                   Forgot your password?
                 </Link>
               </div>
-              <Input id="password" type="password" required />
+              <Input
+                id="password"
+                type="password"
+                placeholder="****"
+                required
+              />
             </div>
             <Button type="submit" className="w-full">
               Login
@@ -47,20 +48,11 @@ export default function page() {
           </div>
           <div className="mt-4 text-center text-sm">
             Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
+            <Link href="/admin/sign-up" className="underline">
               Sign up
             </Link>
           </div>
         </div>
-      </div>
-      <div className="hidden bg-muted lg:block">
-        <Image
-          src="/placeholder.svg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
       </div>
     </div>
   );
